0e7338b600d900bfbf9ea20a82d969f09af5fedc
Daniel Santander <santanderad00@gmail.com>
2025-11-02 20:10:23 -0300

    first commit and project intialization

---
+++ src/test/java/cl/duoc/userms/UsermsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.userms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
