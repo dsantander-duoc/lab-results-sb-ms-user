3650adac588ba746057cffbcff8888dbea0732b7
Daniel Santander <santanderad00@gmail.com>
2025-11-03 22:17:09 -0300

    feat: add logs

--- src/main/java/cl/duoc/userms/controller/AuthController.java
+++ src/main/java/cl/duoc/userms/controller/AuthController.java
@@ -1,5 +1,7 @@
 package cl.duoc.userms.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cl.duoc.userms.dto.LoginRequest;
 import cl.duoc.userms.dto.UserResponse;
-import cl.duoc.userms.entities.User;
 import cl.duoc.userms.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final UserService userService;
 
     public AuthController(UserService userService) {
@@ -23,11 +27,13 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<UserResponse> login(@RequestBody LoginRequest loginRequest) {
+        logger.info("Logging in user: {}", loginRequest);
         UserResponse user = userService.loginUser(loginRequest);
         if (user == null) {
+            logger.error("User not found: {}", loginRequest);
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        logger.info("User logged in successfully: {}", user);
         return ResponseEntity.ok(user);
     }
-
 }
--- src/main/java/cl/duoc/userms/controller/UserController.java
+++ src/main/java/cl/duoc/userms/controller/UserController.java
@@ -3,6 +3,8 @@ package cl.duoc.userms.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,12 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cl.duoc.userms.dto.CreateUserRequest;
 import cl.duoc.userms.dto.UserResponse;
-import cl.duoc.userms.entities.User;
 import cl.duoc.userms.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -31,6 +36,7 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
+        logger.info("Getting all users");
         return ResponseEntity.ok(userService.getAll());
     }
 
@@ -38,6 +44,7 @@ public class UserController {
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
         Optional<UserResponse> user = userService.getById(id);
         if (user.isPresent()) {
+            logger.info("User found: {}", user.get());
             return ResponseEntity.ok(user.get());
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -46,18 +53,21 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
         UserResponse response = userService.saveUser(request);
+        logger.info("User created: {}", response);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody CreateUserRequest request) {
         UserResponse updatedUser = userService.updateUser(id, request);
+        logger.info("User updated: {}", updatedUser);
         return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        logger.info("User deleted: {}", id);
         return ResponseEntity.noContent().build();
     }
 }
